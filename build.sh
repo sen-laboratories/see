90ae272379a0cc755a54434bd6555346bec4e2c1
Gregor Rosenauer <gregor.rosenauer@gmail.com>
2025-06-25 13:05:58 +0200

    fix reference to binary in build script.

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 # a simple wrapper for make, since I found no way to add this to the Makefile template
 # generates templates and copies them to the user settings directory.
 make && \
-./shoji -g && \
+bin/shoji -g && \
 cp -a generated/* ~/config/settings/shoji/templates/
